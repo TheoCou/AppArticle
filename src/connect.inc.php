c55653581934c788cb1d959a47ddf70b8662c005
Théo Courson <theocourson@gmail.com>
2023-01-19 14:47:57 +0100

    new file:   !index.html 	new file:   article.php 	new file:   asset/calendar.svg 	new file:   asset/code.svg 	new file:   asset/cover.png 	new file:   asset/edit-2.svg 	new file:   css/style.css 	new file:   favicon/android-chrome-192x192.png 	new file:   favicon/android-chrome-512x512.png 	new file:   favicon/apple-touch-icon.png 	new file:   favicon/favicon-16x16.png 	new file:   favicon/favicon-32x32.png 	new file:   favicon/favicon.ico 	new file:   favicon/site.webmanifest 	new file:   index.php 	new file:   pub.php 	new file:   readme.md     new file:   search.php 	new file:   src/connect.inc.php 	new file:   src/connect_bdd.inc.php 	new file:   src/header.inc.php 	new file:   src/traitement.inc.php

---
+++ src/connect.inc.php
@@ -0,0 +1,34 @@
+<?php
+    include_once "connect_bdd.inc.php";
+?>
+<?php foreach($_response as $_article):?> <!--itération avec la bdd -->
+    <article>
+        <h2>
+            <a href="article.php?id=<?= $_article["id"]?>">
+            <?= strip_tags($_article['titre'])?></a>
+            <!-- la méthode strip_tags() permet de filtrer les caractères illégaux-->
+        </h2>
+        <p>
+            <?= $_article['contenu'] ?>
+      
+
+        </p>
+        <time datetime="<?= $_article['date_modification']?>">
+            Date de modification : <?=$_article['date_modification']?>
+        </time>
+        <span class="material-symbols-outlined" aria-hidden="true">
+                thumb_up
+            </span>
+            <span class="material-symbols-outlined" aria-hidden="true">
+                thumb_down_off
+            </span>
+            <span class="material-symbols-outlined" aria-hidden="true">
+                favorite
+            </span>
+            <span class="material-symbols-outlined" aria-hidden="true">
+                comment
+        </span>
+
+    </article>
+    
+<?php endforeach; ?>
